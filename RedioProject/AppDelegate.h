ec4e6bb9a289fea12b8e8f7e30cc9c1db7db81c8
garin <garin@yong.local>
2016-01-03 16:12:11 +0800

    Initial Commit

---
+++ RedioProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedioProject
+//
+//  Created by garin on 16/1/3.
+//  Copyright © 2016年 garin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedioProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedioProject
+//
+//  Created by garin on 16/1/3.
+//  Copyright © 2016年 garin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
